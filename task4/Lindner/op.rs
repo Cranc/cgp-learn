e584499cfb38a86ee63b54231335a62711efeb79
Dennis Lindner <lindner_dennis@yahoo.de>
2016-07-20 10:56:42 +0200

    Adding struct Swagger and test

--- task4/Lindner/op.rs
+++ task4/Lindner/op.rs
@@ -1,5 +1,10 @@
+use std::fmt;
+
 fn main() {
     println!("do math result => {:?}", do_math(6, 3));
+
+    let t = Swagger { object: "yo".to_string() };
+    println!("{}", t);
 }
 
 fn do_math<T: std::ops::Mul + std::ops::Add + Copy>
@@ -7,4 +12,14 @@ fn do_math<T: std::ops::Mul + std::ops::Add + Copy>
      y: T)
      -> (<T as std::ops::Add>::Output, <T as std::ops::Mul>::Output) {
     (x + y, x * y)
+}
+
+struct Swagger<T: fmt::Display> {
+    object: T,
+}
+
+impl<T: fmt::Display> fmt::Display for Swagger<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "swag {} yolo", self.object)
+    }
 }
